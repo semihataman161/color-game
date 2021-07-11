1f1b582b38ccbf48f24c7163ffa9de05808937de
semihataman161 <76014928+semihataman161@users.noreply.github.com>
2021-07-12 00:39:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,170 +1,152 @@
- /*
-  * @author Semih Ataman
-  * @since 9 July 2021
-  */
+/*
+ * @author Semih Ataman
+ * @since 9 July 2021
+ */
 
- $("#topOfGrid").hide();
- $("#grid .btn").hide();
- $("#endOfTheGame").hide();
+$("#topOfGrid").hide();
+$("#grid .btn").hide();
+$("#endOfTheGame").hide();
 
- var allColors = ["orange", "purple", "pink", "yellow", "dark-cyan", "blue", "white", "gray",
-   "green", "red", "olive", "brown", "turquoise", "rebecca-purple", "orange-red", "thistle", "lime",
-   "magenta", "peru", "sky-blue", "slate-blue", "saddle-brown", "navy", "salmon", "silver", "sienna"
- ];
+var allColors = ["orange", "purple", "pink", "yellow", "dark-cyan", "blue", "white", "gray",
+  "green", "red", "olive", "brown", "turquoise", "rebecca-purple", "orange-red", "thistle", "lime",
+  "magenta", "peru", "sky-blue", "slate-blue", "saddle-brown", "navy", "salmon", "silver", "sienna"
+];
 
- var seenColors = []; // This is the array of seen colors on the screen.
- var totalPoint = 0;
+var seenColors = []; // This is the array of seen colors on the screen.
+var totalPoint = -10;
 
- $("#difficulty .btn").click(function() {
-   $("#level-title").hide();
-   $("#difficulty").hide();
-   $("#topOfGrid").show();
- });
+let strategies = new Map([
+  ["threeToThree", arrangeThreeToThreeColors],
+  ["fourToFour", arrangeFourToFourColors],
+  ["fiveToFive", arrangeFiveToFiveColors]
+]);
 
+// Thanks to this strategy, I didn't use any if/else or switch-case statement. Instead I used map.
+function action(selection) {
+  let strategy = strategies.get(selection);
+  var nextColor = strategy();
+  return nextColor;
+}
 
- $("#threeToThree").click(function() {
+$("#difficulty .btn").click(function() {
+  $("#level-title").hide();
+  $("#difficulty").hide();
+  $("#topOfGrid").show();
 
-   executeTimer();
-   var nextColor = arrangeThreeToThreeColors();
+  playSound("Start");
+  executeTimer();
 
-   $("#grid .btn").click(function() {
-     if ($(this).attr("id") === nextColor) {
-       playSound("Success");
-       totalPoint += 10;
-       console.log("Total Point: " + totalPoint);
-       seenColors = [];
-       nextColor = arrangeThreeToThreeColors();
-     } else {
-       playSound("Failure");
-     }
-   });
- });
+  var selection = $(this).attr("id");
+  var nextColor = nextSequence(selection);
 
- $("#fourToFour").click(function() {
+  $("#grid .btn").click(function() {
+    animatePress(this);
+    if ($(this).attr("id") === nextColor) {
+      playSound("Success");
+      nextColor = nextSequence(selection);
+    } else {
+      playSound("Failure");
+    }
+  });
+});
 
-   executeTimer();
-   var nextColor = arrangeFourToFourColors();
+function nextSequence(selection) {
+  seenColors = [];
+  action(selection);
+  var nextColor = getRandomColor(seenColors);
+  $("#next").attr("class", "btn next " + nextColor);
+  totalPoint += 10;
+  console.log(totalPoint);
+  return nextColor;
+}
 
-   $("#grid .btn").click(function() {
-     if ($(this).attr("id") === nextColor) {
-       playSound("Success");
-       totalPoint += 10;
-       console.log("Total Point: " + totalPoint);
-       seenColors = [];
-       nextColor = arrangeFourToFourColors();
-     } else {
-       playSound("Failure");
-     }
-   });
- });
+function getRandomColor(colors) {
+  var randomNumber = Math.floor(Math.random() * colors.length);
+  var randomColor = colors[randomNumber];
+  return randomColor;
+}
 
- $("#fiveToFive").click(function() {
+// This function provides that one color can only be taken once.
+function getValidColor() {
+  var randomColor;
+  do {
+    randomColor = getRandomColor(allColors);
+  }
+  while (seenColors.includes(randomColor));
+  return randomColor;
+}
 
-   executeTimer();
-   var nextColor = arrangeFiveToFiveColors();
+function executeTimer() {
+  var counter = 60;
+  $("#timer").html(counter);
+  var terminationId = setInterval(function() {
+    counter--;
+    $("#timer").html(counter);
 
-   $("#grid .btn").click(function() {
-     if ($(this).attr("id") === nextColor) {
-       playSound("Success");
-       totalPoint += 10;
-       console.log("Total Point: " + totalPoint);
-       seenColors = [];
-       nextColor = arrangeFiveToFiveColors();
-     } else {
-       playSound("Failure");
-     }
-   });
- });
+    if (counter == 10) {
+      playSound("TimeRunningOut");
+    }
 
- function getRandomColor(colors) {
-   var randomNumber = Math.floor(Math.random() * colors.length);
-   var randomColor = colors[randomNumber];
-   return randomColor;
- }
+    if (counter < 0) {
+      clearInterval(terminationId);
+      $("#topOfGrid").hide();
+      $("#grid").hide();
+      $("#endOfTheGame").show();
+      $("#point h2").html(totalPoint);
+    }
+  }, 1000);
+}
 
- // This function provides that one color can only be taken once.
- function getValidColor() {
-   var randomColor;
-   do {
-     randomColor = getRandomColor(allColors);
-   }
-   while (seenColors.includes(randomColor));
-   return randomColor;
- }
+function arrangeThreeToThreeColors() {
+  for (let i = 0; i <= 12; i++) {
+    let mod = i % 5;
+    if (mod === 0 || mod === 1 || mod === 2) {
+      var randomColor = getValidColor();
+      manipulateButtons(i, randomColor);
+      seenColors.push(randomColor);
 
- function executeTimer() {
-   var counter = 60;
-   $("#timer").html(counter);
-   var x = setInterval(function() {
-     counter--;
-     $("#timer").html(counter);
+    } else {
+      continue;
+    }
+  }
+}
 
-     if (counter == 10) {
-       playSound("TimeRunningOut");
-     }
+function arrangeFourToFourColors() {
+  for (let i = 0; i <= 18; i++) {
+    let mod = i % 5;
+    if (mod === 0 || mod === 1 || mod === 2 || mod === 3) {
+      var randomColor = getValidColor();
+      manipulateButtons(i, randomColor);
+      seenColors.push(randomColor);
 
-     if (counter < 0) {
-       clearInterval(x);
-       $("#topOfGrid").hide();
-       $("#grid").hide();
-       $("#endOfTheGame").show();
-       $("#point h2").html(totalPoint);
-     }
-   }, 1000);
- }
+    } else {
+      continue;
+    }
+  }
+}
 
- function arrangeThreeToThreeColors() {
-   for (let i = 0; i <= 12; i++) {
-     let mod = i % 5;
-     if (mod === 0 || mod === 1 || mod === 2) {
-       var randomColor = getValidColor();
-       $("#grid .btn:eq(" + i + ")").attr("id", randomColor);
-       $("#grid .btn:eq(" + i + ")").attr("class", "btn " + randomColor);
-       $("#grid .btn:eq(" + i + ")").show();
-       seenColors.push(randomColor);
+function arrangeFiveToFiveColors() {
+  for (let i = 0; i <= 24; i++) {
+    var randomColor = getValidColor();
+    manipulateButtons(i, randomColor);
+    seenColors.push(randomColor);
+  }
+}
 
-     } else {
-       continue;
-     }
-   }
-   var nextColor = getRandomColor(seenColors);
-   $("#next").attr("class", "btn next " + nextColor);
-   return nextColor;
- }
+function manipulateButtons(index, randomColor) {
+  $("#grid .btn:eq(" + index + ")").attr("id", randomColor);
+  $("#grid .btn:eq(" + index + ")").attr("class", "btn " + randomColor);
+  $("#grid .btn:eq(" + index + ")").show();
+}
 
- function arrangeFourToFourColors() {
-   for (let i = 0; i <= 18; i++) {
-     let mod = i % 5;
-     if (mod === 0 || mod === 1 || mod === 2 || mod === 3) {
-       var randomColor = getValidColor();
-       $("#grid .btn:eq(" + i + ")").attr("id", randomColor);
-       $("#grid .btn:eq(" + i + ")").attr("class", "btn " + randomColor);
-       $("#grid .btn:eq(" + i + ")").show();
-       seenColors.push(randomColor);
+function playSound(name) {
+  var audio = new Audio("Sounds/" + name + ".flac");
+  audio.play();
+}
 
-     } else {
-       continue;
-     }
-   }
-   var nextColor = getRandomColor(seenColors);
-   $("#next").attr("class", "btn next " + nextColor);
-   return nextColor;
- }
-
- function arrangeFiveToFiveColors() {
-   for (let i = 0; i <= 24; i++) {
-     var randomColor = getValidColor();
-     $("#grid .btn:eq(" + i + ")").attr("id", randomColor);
-     $("#grid .btn:eq(" + i + ")").attr("class", "btn " + randomColor);
-     $("#grid .btn:eq(" + i + ")").show();
-     seenColors.push(randomColor);
-   }
-   var nextColor = getRandomColor(seenColors);
-   $("#next").attr("class", "btn next " + nextColor);
-   return nextColor;
- }
-
- function playSound(name) {
-   var audio = new Audio("Sounds/" + name + ".flac");
-   audio.play();
- }
+function animatePress(clickedButton) {
+  $(clickedButton).addClass("pressed");
+  setTimeout(function() {
+    $(clickedButton).removeClass("pressed");
+  }, 100);
+}
